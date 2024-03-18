3a8eec798f462ed3c54038ec222e7965e6e7f469
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-03-18 11:04:11 +0530

    Add

--- src/Redux/itemSlice.js
+++ src/Redux/itemSlice.js
@@ -14,7 +14,6 @@ const itemSlice = createSlice({
     getPost: (state, action) => {
       ({ ...state, ...action.payload });
     },
-    
     // getPost: (state, action) => {
     //   state.db.push(action.payload);
     // },
