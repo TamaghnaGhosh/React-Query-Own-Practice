6cd5a838d6a531cb7a5a6584f2ff08a0e94ef320
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-03-19 11:03:29 +0530

    Space added

--- src/Redux/itemSlice.js
+++ src/Redux/itemSlice.js
@@ -14,6 +14,7 @@ const itemSlice = createSlice({
     getPost: (state, action) => {
       ({ ...state, ...action.payload });
     },
+    
     // getPost: (state, action) => {
     //   state.db.push(action.payload);
     // },
